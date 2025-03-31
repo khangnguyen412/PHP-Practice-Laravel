636a0dd249acf221b44ad663f43f993cf5c4ebb9
khangnguyen412 <119936631+khangnguyen412@users.noreply.github.com>
2025-03-31 07:34:21 +0700

    7h 31/03/2025: update branch main

--- Laravel-Learning/app/Http/Controllers/Lecture20/ControllerLecture20.php
+++ Laravel-Learning/app/Http/Controllers/Lecture20/ControllerLecture20.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers\Lecture20;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Auth\Events\Validated;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 
@@ -17,14 +16,18 @@ class ControllerLecture20 extends Controller
     public function validations_form(Request $request)
     {
         /**
-         *  
+         *  - Import thư viện Illuminate\Support\Facades\Validator
+         *  - Cú pháp Validator::make
+         */
+        /**
+         *  - Các rule: 
          */
         $valid_data = Validator::make(
             $request->all(),
             [
                 'username'      => 'required|min:5|max:255',
                 'email'         => 'required|email',
-                'phone'         => 'required|integer',
+                'phone'         => 'required|numeric',
                 'city'          => 'required|max:255',
             ],
             [
@@ -40,9 +43,24 @@ class ControllerLecture20 extends Controller
                 'city'          => 'Thành phố',
             ],
         );
+
+        /**
+         *  - Các method của validator:
+         *  + passes(): Kiểm tra dữ liệu có hợp lệ hay không. Trả về true nếu tất cả các rule đều thỏa mãn.
+         *  + fails(): Kiểm tra xem dữ liệu có vi phạm bất kỳ rule nào không. Trả về true nếu có lỗi.
+         *  + errors(): Lấy danh sách lỗi dạng MessageBag. Cho phép hiển thị lỗi cho từng field.
+         *  + validated(): Trả về mảng dữ liệu đã được xác thực. Chỉ có khi validate thành công, dùng trong lớp Form Request.
+         *  + sometimes(): Cho phép thêm rule conditionally cho các field.
+         *  + failed(): Trả về mảng các rule đã bị vi phạm cho mỗi field.
+         */
         if ($valid_data->fails()){
             $errors = json_encode($valid_data->errors(), JSON_UNESCAPED_UNICODE);
             return response($errors)->header('Content-Type', 'application/json; charset=UTF-8');
         }
+        if ($valid_data->passes()){
+            $result = json_encode($valid_data->validated(), JSON_UNESCAPED_UNICODE);
+            return response($result)->header('Content-Type', 'application/json; charset=UTF-8');
+        }
+        
     }
 }
