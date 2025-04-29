909922ac5ea74b2ee58fe270022da1cb805e8482
khangnguyen412 <119936631+khangnguyen412@users.noreply.github.com>
2025-04-29 13:54:33 +0700

    1h 29/04/2025: - update docker

--- start-laravel.bat
+++ start-laravel.bat
@@ -1,20 +1,22 @@
-:: Vào đường dẫn
+@REM Vào đường dẫn
+cd "%USERPROFILE%\Desktop\Practice Laravel"
 cd .\Laravel-Learning\
 
-:: Xoá các img cũ (chỉ lần cài đặt đầu tiên)
-:: không nên chạy system prune nếu còn những lưu container khác
-:: docker system prune -a -f 
-:: docker image prune -a -f
+@REM Xoá các img cũ (chỉ lần cài đặt đầu tiên)
+@REM không nên chạy system prune nếu còn những lưu container khác
+@REM docker system prune -a -f 
+@REM docker image prune -a -f
+@REM docker volume prune -a -f
 
-:: Chạy docker-compose (chỉ lần cài đặt đầu tiên)
-:: docker compose up -d
+@REM Chạy docker-compose (chỉ lần cài đặt đầu tiên)
+@REM docker compose up -d
 
-:: Cài composer (chỉ lần cài đặt đầu tiên)
-:: docker exec Laravel-App composer install
+@REM Cài composer (chỉ lần cài đặt đầu tiên)
+@REM docker exec Laravel-App composer install
 
-:: Chạy migrate cho source (chỉ lần cài đặt đầu tiên, cấu hình file .env trước khi chạy, nếu muốn tạo ra các bảng cần thiết)
-:: docker-compose exec app php artisan migrate
+@REM Chạy migrate cho source (chỉ lần cài đặt đầu tiên, cấu hình file .env trước khi chạy, nếu muốn tạo ra các bảng cần thiết)
+@REM docker-compose exec app php artisan migrate
 
-:: Khởi động project
+@REM :: Khởi động project
 docker restart Laravel-App Laravel-Webserver Laravel-Sql
 docker exec Laravel-App php artisan serve --host=0.0.0.0 --port=8000
